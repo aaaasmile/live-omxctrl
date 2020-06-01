595b9ced3d0cf4c0dc506f520645ed0b91698607
aaaasmile <testhaus@gmx.at>
2020-06-01 23:11:41 +0200

    added some powershell scripts

---
+++ publish.ps1
@@ -0,0 +1,20 @@
+$outarm = "live-omxctrl.bin"
+$confirmation = Read-Host "Do you want to build a new version target raspberry pi4 (y/n)?"
+if ($confirmation -ne 'y') {
+    Write-Host "Nothing to do. All the best."
+    return
+}
+
+#create the exe
+Write-Host "Build the $outarm in $pwd"
+$env:GOOS = "linux"
+$env:GOARCH="arm"
+$env:GOARM="5"
+go build -o $outarm
+
+# Create the Zip package
+Write-Host "Create a deploy package"
+cd ./deploy
+.\deploy.exe -target pi4
+
+Write-Host "Done. Now the process continue with the WLC using the batch copy_app_to_pi4.sh"
--- web/idl/idl.go
+++ web/idl/idl.go
@@ -2,5 +2,5 @@ package idl
 
 var (
 	Appname = "live-omxctrl"
-	Buildnr = "00.01.01.20200601-02"
+	Buildnr = "00.01.02.20200601-03"
 )
