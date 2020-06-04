eafd67b1be4e0d42e9681c2079651bc48f707d5a
aaaasmile <testhaus@gmx.at>
2020-06-04 02:08:17 +0200

    update

--- static/js/vue/views/dashboard.js
+++ static/js/vue/views/dashboard.js
@@ -8,7 +8,7 @@ export default {
     template: `
   <v-card color="grey lighten-4" flat tile>
     <v-toolbar flat dense>
-      <v-toolbar-title class="subheading grey--text">Dashboard</v-toolbar-title>
+      <v-toolbar-title class="subheading grey--text">Dashboard for Omx Player</v-toolbar-title>
      </v-toolbar>
-  </v-card>`
+  </v-card>`
 }
