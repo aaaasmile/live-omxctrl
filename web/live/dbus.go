2d3ada7a3f5c72bd067f6267044d64ef002a11ae
aaaasmile <testhaus@gmx.at>
2020-06-05 21:14:52 +0200

    works duration request

--- web/live/dbus.go
+++ web/live/dbus.go
@@ -10,17 +10,38 @@ import (
 	"github.com/godbus/dbus"
 )
 
+var sdbus *dbus.Conn
+
 func testSomeDbus() error {
 	log.Println("Let's try some dbus")
 	//busAddr := "unix:abstract=/tmp/dbus-1OTLRLIFgE,guid=39be549b2196c379ccdf29585ed9674d"
 	//opath := dbus.ObjectPath(busAddr)
+	var err error
+	if sdbus == nil {
+		sdbus, err = connectDbBus()
+		if err != nil {
+			return err
+		}
+	}
+
+	obj := sdbus.Object("org.mpris.MediaPlayer2.omxplayer", "/org/mpris/MediaPlayer2/omxplayer")
+	log.Println("Object is ", obj)
+	dur, err := obj.GetProperty("org.mpris.MediaPlayer2.Player.Duration")
+	if err != nil {
+		return err
+	}
+	log.Println("Duration is ", dur)
+	return nil
+}
+
+func connectDbBus() (*dbus.Conn, error) {
 	u, err := user.Current()
 	log.Println("User ", u.Username)
 	fname := fmt.Sprintf("/tmp/omxplayerdbus.%s", u.Username)
 	if _, err := os.Stat(fname); err == nil {
 		raw, err := ioutil.ReadFile(fname)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		os.Setenv("DBUS_SESSION_BUS_ADDRESS", string(raw))
 		log.Println("Env DBUS_SESSION_BUS_ADDRESS set to ", string(raw))
@@ -28,44 +49,33 @@ func testSomeDbus() error {
 
 	conn, err := dbus.SessionBus()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	address := os.Getenv("DBUS_SESSION_BUS_ADDRESS")
 	log.Println("session bus addr ", address)
 	log.Println("dbus connection: ", conn)
 
-	// var s []string
-	// err = conn.Object.Call("/org/mpris/MediaPlayer2")
-	// //.Ge org.freedesktop.DBus.Properties.Get", 0).Store(&s)
-	// if err != nil {
-	// 	return fmt.Errorf("Failed to get list of owned names: %v", err)
-	// }
-	// log.Println("Currently owned names on the session bus:")
-	// for _, v := range s {
-	// 	fmt.Println(v)
-	// }
-
-	return nil
+	return conn, nil
 }
 
-func testSomeDbus1() error {
-	log.Println("Let's try some dbus")
-	conn, err := dbus.SessionBus()
-	if err != nil {
-		return err
-	}
-	log.Println("dbus connection: ", conn)
-	defer conn.Close()
+// func testSomeDbus1() error {
+// 	log.Println("Let's try some dbus")
+// 	conn, err := dbus.SessionBus()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	log.Println("dbus connection: ", conn)
+// 	defer conn.Close()
 
-	var s []string
-	err = conn.BusObject().Call("org.freedesktop.DBus.ListNames", 0).Store(&s)
-	if err != nil {
-		return fmt.Errorf("Failed to get list of owned names: %v", err)
-	}
-	log.Println("Currently owned names on the session bus:")
-	for _, v := range s {
-		fmt.Println(v)
-	}
+// 	var s []string
+// 	err = conn.BusObject().Call("org.freedesktop.DBus.ListNames", 0).Store(&s)
+// 	if err != nil {
+// 		return fmt.Errorf("Failed to get list of owned names: %v", err)
+// 	}
+// 	log.Println("Currently owned names on the session bus:")
+// 	for _, v := range s {
+// 		fmt.Println(v)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
