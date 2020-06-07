d158618745caf7366ffa65e57d69c5eb26997a43
aaaasmile <testhaus@gmx.at>
2020-06-07 20:46:03 +0200

    update install script

---
+++ publish-pi4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Buils app"
+go build -o live-omxctrl.bin
+
+cd ./deploy
+
+echo "build the zip package"
+./deploy.bin -target pi4 -outdir ~/app/live-omxctrl/zips/
+cd ~/app/live-omxctrl/
+
+echo "update the service"
+./update-service.sh
+
+echo "Ready to fly"
