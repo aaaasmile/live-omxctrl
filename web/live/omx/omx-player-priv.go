2931fdca72f207c301322e8f3fffee7a0c4d976d
aaaasmile <testhaus@gmx.at>
2020-12-13 20:32:16 +0100

    update but not ok

--- web/live/omx/omx-player-priv.go
+++ web/live/omx/omx-player-priv.go
@@ -1,12 +1,15 @@
 package omx
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"os/user"
+	"sync"
 
 	"github.com/aaaasmile/live-omxctrl/web/idl"
 	"github.com/aaaasmile/live-omxctrl/web/live/omx/playlist"
@@ -28,19 +31,76 @@ type actionDef struct {
 	Action actionTD
 }
 
+// CapturingPassThroughWriter is a writer that remembers
+// data written to it and passes it to w
+type CapturingPassThroughWriter struct {
+	buf bytes.Buffer
+	w   io.Writer
+}
+
+// NewCapturingPassThroughWriter creates new CapturingPassThroughWriter
+func NewCapturingPassThroughWriter(w io.Writer) *CapturingPassThroughWriter {
+	return &CapturingPassThroughWriter{
+		w: w,
+	}
+}
+
+func (w *CapturingPassThroughWriter) Write(d []byte) (int, error) {
+	w.buf.Write(d)
+	return w.w.Write(d)
+}
+
+// Bytes returns bytes written to the writer
+func (w *CapturingPassThroughWriter) Bytes() []byte {
+	return w.buf.Bytes()
+}
+
 func (op *OmxPlayer) execCommand(uri string) {
 	log.Println("Prepare to start the player with execCommand")
-	go func(cmdOmx *exec.Cmd, actCh chan *actionDef, uri string) {
+	go func(cmd *exec.Cmd, actCh chan *actionDef, uri string) {
+		// op.cmdOmx = exec.Command("bash", "-c", cmd)
 		actCh <- &actionDef{
 			URI:    uri,
 			Action: actPlaying,
 		}
-		out, err := cmdOmx.Output()
-		log.Println("Command out ", string(out))
+		// out, err := cmdOmx.Output()
+		// log.Println("Command out ", string(out))
+		// if err != nil {
+		// 	log.Println("Command executed with error: ", err)
+		// }
+		var errStdout, errStderr error
+		stdoutIn, _ := cmd.StdoutPipe()
+		stderrIn, _ := cmd.StderrPipe()
+		stdout := NewCapturingPassThroughWriter(os.Stdout)
+		stderr := NewCapturingPassThroughWriter(os.Stderr)
+
+		err := cmd.Start()
 		if err != nil {
-			log.Println("Command executed with error: ", err)
+			log.Fatalf("cmd.Start() failed with '%s'\n", err)
 		}
-		log.Println("Closing player with ", cmdOmx)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		go func() {
+			_, errStdout = io.Copy(stdout, stdoutIn)
+			wg.Done()
+		}()
+
+		_, errStderr = io.Copy(stderr, stderrIn)
+		wg.Wait()
+
+		err = cmd.Wait()
+		if err != nil {
+			log.Println("ERROR:  cmd.Run() failed with", err)
+		}
+		if errStdout != nil || errStderr != nil {
+			log.Println("ERROR: failed to capture stdout or stderr")
+		}
+		outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+		fmt.Printf("\nout:\n%s\nerr:\n%s\n", outStr, errStr)
+
+		log.Println("Closing player with ", cmd)
 		actCh <- &actionDef{
 			URI:    uri,
 			Action: actTerminate,
